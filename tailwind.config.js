a084a29822a0ef9c306619059956f59f319d9925
loomrkt <mrnrakotondrazaka@gmail.com>
2023-12-09 10:53:43 +0200

    2section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Primary-01": "#1B3764",
+        "Primary-02": "#FFCA42",
+        "Primary-03": "#F6F8FC",
+      },
+      fontFamily: {
+        Cardio: "Cardio",
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
