42803fb223ecb702f587ec19d6252e5372f205d2
loomrkt <mrnrakotondrazaka@gmail.com>
2023-12-10 10:02:09 +0200

    need animation

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const preload = document.querySelector('#preload');
+    preload.style.opacity = 0;
+    preload.style.visibility = 'hidden';
+    document.body.style.overflowY = 'scroll';
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         "Primary-02": "#FFCA42",
         "Primary-03": "#F6F8FC",
         "Primary-04": "#F4F8FF",
+        "Secondary-01": "#B4C7E7",
+        "Secondary-02":  "#969AA0",
+        "Secondary-03":  "#1B3764",
       },
       fontFamily: {
         Cardo: "Cardo",
